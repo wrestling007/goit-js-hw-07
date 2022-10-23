010929b2b9fc4b17161ebac1835d77fe1210c826
Viacheslav ALEKSIEIEV <bagetta007@gmail.com>
2022-10-23 19:54:44 +0300

    finish

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,24 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryRef = document.querySelector('.gallery');
+const itemGallery = createGalleryItems(galleryItems);
+galleryRef.innerHTML = itemGallery;
+
+function createGalleryItems(galleryItems) {
+    return galleryItems
+        .map(item => 
+        ` <li>
+        <a class="gallery__item" href="${item.original}">
+        <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a>
+</li>`
+    )
+    .join('');
+}
+
+new SimpleLightbox('.gallery__item', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});

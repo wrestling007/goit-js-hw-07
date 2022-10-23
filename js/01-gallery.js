47e6cfc4cbd57f2aafe61f43f595fd493e99d0fb
Viacheslav ALEKSIEIEV <bagetta007@gmail.com>
2022-10-23 16:09:43 +0300

    first

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,45 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
+// Change code below this line
 console.log(galleryItems);
+
+const galleryRef = document.querySelector('.gallery');
+const itemGallery = createGalleryItems(galleryItems);
+galleryRef.innerHTML = itemGallery;
+
+function createGalleryItems(galleryItems) {
+    return galleryItems
+        .map((item) => {
+            return `<div class="gallery__item">
+ <a class="gallery__link" href="${item.original}">
+ <img class="gallery__image"
+ srs="${item.preview}"
+ data-sourse="${item.original}"
+ alt="${item.description}"/>
+ </a>
+ </div>`}).join('');
+};
+
+galleryRef.addEventListener('click', imageClick);
+
+function imageClick(evt) {
+
+    blockAction(evt);
+
+    if (evt.target.nodeName !== 'IMG') {
+        return;
+    } 
+    const instance = basicLightbox.create(`
+    <img src="assets/images/image.png" width="800" height="600">
+`)
+
+    instance.show();
+    galleryRef.addEventListener('keydown', (evt) => {
+        if (evt.code === 'Escape') {
+            instance.close();
+        }
+    });
+}
+function blockAction(evt) {
+    evt.preventDefault();
+}
